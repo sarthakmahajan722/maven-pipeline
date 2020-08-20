9a5aec08ffd4c1e142e27874efa5059c18431438
sarthak mahajan <sarthak.mahajan1234@gmail.com>
2020-08-20 16:32:10 +0530

    push.sh modified

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,7 +7,7 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u ricardoandre97 -p $PASS
+docker login -u sarthakmahajan722 -p $PASS
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG sarthakmahajan722/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
